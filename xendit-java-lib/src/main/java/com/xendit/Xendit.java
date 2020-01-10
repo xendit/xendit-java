be4a7493ffe286fb26a514305bc45d4f9a6ce881
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2020-01-10 13:49:20 +0700

    Change invoice unit test implementation

--- xendit-java-lib/src/main/java/com/xendit/Xendit.java
+++ xendit-java-lib/src/main/java/com/xendit/Xendit.java
@@ -1,11 +1,12 @@
 package com.xendit;
 
+import com.xendit.network.BaseRequest;
 import com.xendit.network.NetworkClient;
 
 public class Xendit {
     public static final String LIVE_URL = "https://api.xendit.co";
 
-    public static volatile NetworkClient requestClient;
+    public static volatile NetworkClient requestClient = new BaseRequest();
 
     public static volatile String apiKey;
 
--- xendit-java-lib/src/test/java/com/xendit/model/InvoiceTest.java
+++ xendit-java-lib/src/test/java/com/xendit/model/InvoiceTest.java
@@ -2,15 +2,12 @@ package com.xendit.model;
 
 import com.xendit.Xendit;
 import com.xendit.exception.XenditException;
+import com.xendit.network.BaseRequest;
+import com.xendit.network.RequestResource;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.internal.verification.VerificationModeFactory;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,210 +15,145 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.mockito.Mockito.*;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(Invoice.class)
 public class InvoiceTest {
-    private static final String SAMPLE_KEY = "xnd_development_O46JfOtygef9kMNsK+ZPGT+ZZ9b3ooF4w3Dn+R1k+2fT/7GlCAN3jg==:";
-    private static final String INVALID_KEY = "xnd_development_...";
+    private static final String URL_V2 = String.format("%s%s", Xendit.getUrl(), "/v2/invoices");
+    private static final String URL_V1 = String.format("%s%s", Xendit.getUrl(), "/invoices");
+    private static Map<String, Object> PARAMS = new HashMap<>();
+    private static String TEST_ID = "5e0cb0bbf4d38b20d5421b72";
+    private static String TEST_EXTERNAL_ID = "test_id";
+    private static Number TEST_AMOUNT = 100000;
+    private static Number TEST_AMOUNT_LESS = 1000;
+    private static Number TEST_AMOUNT_GREATER = 1000000001;
+    private static String TEST_EMAIL = "test@email.com";
+    private static String TEST_DESC = "Testing";
+    private static Invoice VALID_INVOICE = Invoice.builder()
+            .id(TEST_ID)
+            .externalId(TEST_EXTERNAL_ID)
+            .amount(TEST_AMOUNT)
+            .payerEmail(TEST_EMAIL)
+            .description(TEST_DESC)
+            .build();
 
-    private Invoice initExpiredInvoice() {
-        return Invoice.builder().status("EXPIRED").build();
+    @Before
+    public void initMocks() {
+        Xendit.requestClient = Mockito.mock(BaseRequest.class);
+        PARAMS.put("external_id", TEST_EXTERNAL_ID);
+        PARAMS.put("amount", TEST_AMOUNT);
+        PARAMS.put("payer_email", TEST_EMAIL);
+        PARAMS.put("description", TEST_DESC);
     }
 
     @Test
-    public void create_Success_IfApiKeyIsValid() throws Exception {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        Invoice invoiceResult = Invoice.builder()
-                .externalId("test_id")
-                .amount(100000)
-                .payerEmail("test@email.com")
-                .description("Testing")
-                .build();
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create("test_id", 100000, "test@email.com", "Testing")).thenReturn(invoiceResult);
-        Invoice invoice = Invoice.create("test_id", 100000, "test@email.com", "Testing");
-        Class invoiceClass = invoice.getClass();
-        Method request = invoiceClass.getDeclaredMethod("request");
-//        Mockito.verify(Invoice);
-//        when(Invoice.create("test_id", 100000, "test@email.com", "Testing")).thenReturn(invoiceMock);
-//        PowerMockito.verifyStatic(VerificationModeFactory.times(1));
-//        assertNotNull(invoice);
-//        assertThat(invoice, instanceOf(Invoice.class));
-    }
-
-    @Test
-    public void create_Success_IfParamsAreValid() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("external_id", "test_id");
-        params.put("amount", 100000);
-        params.put("payer_email", "test@email.com");
-        params.put("description", "Testing");
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create(params)).thenReturn(Invoice.builder().build());
-        Invoice invoice = Invoice.create(params);
-        assertNotNull(invoice);
-        assertThat(invoice, instanceOf(Invoice.class));
-    }
-
-    @Test(expected = XenditException.class)
-    public void create_ThrowsException_IfApiKeyIsInvalid() throws XenditException {
-        Xendit.apiKey = INVALID_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create("test_id", 100000, "test@email.com", "Testing")).thenThrow(XenditException.class);
-        Invoice.create("test_id", 100000, "test@email.com", "Testing");
+    public void create_Success_IfParamsAreValid() throws Exception {
+        when(Xendit.requestClient.request(RequestResource.Method.POST, URL_V2, PARAMS, Invoice.class))
+                .thenReturn(VALID_INVOICE);
+        Invoice invoice = Invoice.create(TEST_EXTERNAL_ID, TEST_AMOUNT, TEST_EMAIL, TEST_DESC);
+        assertEquals(invoice, VALID_INVOICE);
     }
 
     @Test(expected = XenditException.class)
     public void create_ThrowsException_IfAmountIsLessThanMinimum() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create("test_id", 1000, "test@email.com", "Testing")).thenThrow(XenditException.class);
-        Invoice.create("test_id", 1000, "test@email.com", "Testing");
+        PARAMS.put("amount", TEST_AMOUNT_LESS);
+        when(Xendit.requestClient.request(RequestResource.Method.POST, URL_V2, PARAMS, Invoice.class))
+                .thenThrow(XenditException.class);
+        Invoice.create(TEST_EXTERNAL_ID, TEST_AMOUNT_LESS, TEST_EMAIL, TEST_DESC);
     }
 
     @Test(expected = XenditException.class)
     public void create_ThrowsException_IfAmountIsGreaterThanMaximum() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create("test_id", 1000000001, "test@email.com", "Testing")).thenThrow(XenditException.class);
-        Invoice.create("test_id", 1000000001, "test@email.com", "Testing");
+        PARAMS.put("amount", TEST_AMOUNT_GREATER);
+        when(Xendit.requestClient.request(RequestResource.Method.POST, URL_V2, PARAMS, Invoice.class))
+                .thenThrow(XenditException.class);
+        Invoice.create(TEST_EXTERNAL_ID, TEST_AMOUNT_GREATER, TEST_EMAIL, TEST_DESC);
     }
 
     @Test(expected = XenditException.class)
     public void create_ThrowsException_IfInvalidParams() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
         Map<String, Object> params = new HashMap<>();
-        params.put("externals_id", "test_id");
-        params.put("amount", 100000);
-        params.put("description", "Testing");
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.create(params)).thenThrow(XenditException.class);
+        when(Xendit.requestClient.request(RequestResource.Method.POST, URL_V2, params, Invoice.class))
+                .thenThrow(XenditException.class);
         Invoice.create(params);
     }
 
     @Test
     public void getById_Success_IfIdIsAvailable() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getById("5e0cb0bbf4d38b20d5421b72")).thenReturn(Invoice.builder().build());
-        Invoice invoice = Invoice.getById("5e0cb0bbf4d38b20d5421b72");
-        assertNotNull(invoice);
-        assertThat(invoice, instanceOf(Invoice.class));
-    }
-
-    @Test(expected = XenditException.class)
-    public void getById_ThrowsException_IfApiKeyIsInvalid() throws XenditException {
-        Xendit.apiKey = INVALID_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getById("5e0cb0bbf4d38b20d5421b72")).thenThrow(XenditException.class);
-        Invoice.getById("5e0cb0bbf4d38b20d5421b72");
+        String url = String.format("%s%s%s", URL_V2, "/", TEST_ID);
+        when(Xendit.requestClient.request(RequestResource.Method.GET, url, null, Invoice.class))
+                .thenReturn(VALID_INVOICE);
+        Invoice invoice = Invoice.getById(TEST_ID);
+        assertEquals(invoice.getId(), VALID_INVOICE.getId());
     }
 
     @Test(expected = XenditException.class)
     public void getById_ThrowsException_IfIdIsNotAvailable() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getById("012345679")).thenThrow(XenditException.class);
-        Invoice.getById("012345679");
+        String url = String.format("%s%s%s", URL_V2, "/", "fake_id");
+        when(Xendit.requestClient.request(RequestResource.Method.GET, url, null, Invoice.class))
+                .thenThrow(XenditException.class);
+        Invoice.getById("fake_id");
     }
 
     @Test
     public void getAll_Success_IfParamsAreValid() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
+        Number limit = 2;
+        String statuses = "[\"SETTLED\",\"EXPIRED\"]";
+        String clientTypes = "[\"DASHBOARD\",\"API_GATEWAY\"]";
+        String lastInvoiceId = "5ca186e407f3b83e34176eac";
+        String after = "2016-02-24T23:48:36.697Z";
+        String before = "2020-02-24T23:48:36.697Z";
 
         Map<String, Object> params = new HashMap<>();
-        params.put("limit", 2);
-        params.put("statuses", "[\"SETTLED\",\"EXPIRED\"]");
-        params.put("client_types", "[\"DASHBOARD\",\"API_GATEWAY\"]");
-        params.put("last_invoice_id", "5ca186e407f3b83e34176eac");
-        params.put("after", "2016-02-24T23:48:36.697Z");
-        params.put("before", "2020-02-24T23:48:36.697Z");
+        params.put("limit", limit);
+        params.put("statuses", statuses);
+        params.put("client_types", clientTypes);
+        params.put("last_invoice_id", lastInvoiceId);
+        params.put("after", after);
+        params.put("before", before);
 
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getAll(params)).thenReturn(new Invoice[]{});
+        String urlParams = String.format("%s%s%s%s%s%s",
+                String.format("%s%s", "&limit=", limit.toString()),
+                String.format("%s%s", "&statuses=", statuses),
+                String.format("%s%s", "&last_invoice_id=", lastInvoiceId),
+                String.format("%s%s", "&client_types=", clientTypes),
+                String.format("%s%s", "&after=", after),
+                String.format("%s%s", "&before=", before)
+        );
+        String url = String.format("%s%s%s", URL_V2, "?", urlParams);
+
+        Invoice[] invoiceResult = {VALID_INVOICE};
+
+        when(Xendit.requestClient.request(RequestResource.Method.GET, url, null, Invoice[].class))
+                .thenReturn(invoiceResult);
         Invoice[] invoices = Invoice.getAll(params);
-        assertNotNull(invoices);
-        assertThat(invoices, instanceOf(Invoice[].class));
-    }
-
-    @Test
-    public void getAll_Success_IfParamsAreValidButIncomplete() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-        Map<String, Object> params = new HashMap<>();
-        params.put("limit", 2);
-        params.put("statuses", "[\"SETTLED\",\"EXPIRED\"]");
-        params.put("last_invoice_id", "5ca186e407f3b83e34176eac");
-        params.put("before", "2020-02-24T23:48:36.697Z");
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getAll(params)).thenReturn(new Invoice[]{});
-        Invoice[] invoices = Invoice.getAll(params);
-        assertNotNull(invoices);
+        assertEquals(invoices[0], VALID_INVOICE);
         assertThat(invoices, instanceOf(Invoice[].class));
     }
 
     @Test
     public void getAll_Success_IfNoGivenParams() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-        Map<String, Object> params = new HashMap<>();
+        Invoice[] invoiceResult = {VALID_INVOICE};
+        String url = String.format("%s%s", URL_V2, "?");
 
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getAll(params)).thenReturn(new Invoice[]{});
-        Invoice[] invoices = Invoice.getAll(params);
-        assertNotNull(invoices);
+        when(Xendit.requestClient.request(RequestResource.Method.GET, url, null, Invoice[].class))
+                .thenReturn(invoiceResult);
+        Invoice[] invoices = Invoice.getAll(new HashMap<>());
+        assertEquals(invoices[0], VALID_INVOICE);
         assertThat(invoices, instanceOf(Invoice[].class));
-    }
-
-    @Test(expected = XenditException.class)
-    public void getAll_ThrowsException_IfApiKeyIsInvalid() throws XenditException {
-        Xendit.apiKey = INVALID_KEY;
-
-        Map<String, Object> params = new HashMap<>();
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.getAll(params)).thenThrow(XenditException.class);
-        Invoice.getAll(params);
     }
 
     @Test
     public void expire_Success_IfIdIsAvailable() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.expire("5e0cb0bbf4d38b20d5421b72")).thenReturn(initExpiredInvoice());
-        Invoice expiredInvoice = Invoice.expire("5e0cb0bbf4d38b20d5421b72");
-        assertNotNull(expiredInvoice);
-        assertThat(expiredInvoice, instanceOf(Invoice.class));
-        assertEquals(expiredInvoice.getStatus(), "EXPIRED");
-    }
-
-    @Test(expected = XenditException.class)
-    public void expire_ThrowsException_IfApiKeyIsInvalid() throws XenditException {
-        Xendit.apiKey = INVALID_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.expire("my_id")).thenThrow(XenditException.class);
-        Invoice.expire("my_id");
+        String url = String.format("%s%s%s%s", URL_V1, "/", TEST_ID, "/expire!");
+        when(Xendit.requestClient.request(RequestResource.Method.POST, url, null, Invoice.class))
+                .thenReturn(VALID_INVOICE);
+        Invoice invoice = Invoice.expire("5e0cb0bbf4d38b20d5421b72");
+        assertEquals(invoice, VALID_INVOICE);
     }
 
     @Test(expected = XenditException.class)
     public void expire_ThrowsException_IfIdIsNotAvailable() throws XenditException {
-        Xendit.apiKey = SAMPLE_KEY;
-
-        PowerMockito.mockStatic(Invoice.class);
-        when(Invoice.expire("123456")).thenThrow(XenditException.class);
-        Invoice.expire("123456");
+        String url = String.format("%s%s", URL_V1, "/fake_id/expire!");
+        when(Xendit.requestClient.request(RequestResource.Method.POST, url, null, Invoice.class))
+                .thenThrow(XenditException.class);
+        Invoice.expire("fake_id");
     }
 }
