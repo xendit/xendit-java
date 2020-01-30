c5f8dcb53adff533a8aab6d785a88f449ba028f1
ahmadfahmip <ahmad.fahmi.pratama@gmail.com>
2020-01-30 15:36:26 +0700

    Added cardless credit objects

---
+++ xendit-java-lib/src/main/java/com/xendit/model/CardlessCreditShippingAddress.java
@@ -0,0 +1,32 @@
+package com.xendit.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class CardlessCreditShippingAddress {
+  @SerializedName("first_name")
+  private String firstName;
+
+  @SerializedName("last_name")
+  private String lastName;
+
+  @SerializedName("address")
+  private String address;
+
+  @SerializedName("city")
+  private String city;
+
+  @SerializedName("postal_code")
+  private String postalCode;
+
+  @SerializedName("phone")
+  private String phone;
+
+  @SerializedName("country_code")
+  private String countryCode;
+}
