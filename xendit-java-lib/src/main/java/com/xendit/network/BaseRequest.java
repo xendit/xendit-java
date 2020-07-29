0940467a94443fe0cabf83fe761f5c3299ef46ec
felix <25915239+f3l1xss@users.noreply.github.com>
2020-07-29 15:48:47 +0700

    fix: Removed Redundant allowHttpMethod() to prevent NoSuchField Exception on Java 12 https://github.com/xendit/xendit-java/issues/58

--- xendit-java-lib/src/main/java/com/xendit/network/BaseRequest.java
+++ xendit-java-lib/src/main/java/com/xendit/network/BaseRequest.java
@@ -106,8 +106,6 @@ public class BaseRequest implements NetworkClient {
     HttpURLConnection connection = null;
 
     try {
-      allowHttpMethods(method.getText());
-
       connection = createXenditConnection(url, apiKey, headers);
 
       connection.setRequestMethod(method.getText());
@@ -137,27 +135,6 @@ public class BaseRequest implements NetworkClient {
       if (connection != null) {
         connection.disconnect();
       }
-    }
-  }
-
-  private static void allowHttpMethods(String... methods) {
-    try {
-      Field methodsField = HttpURLConnection.class.getDeclaredField("methods");
-
-      Field modifiersField = Field.class.getDeclaredField("modifiers");
-      modifiersField.setAccessible(true);
-      modifiersField.setInt(methodsField, methodsField.getModifiers() & ~Modifier.FINAL);
-
-      methodsField.setAccessible(true);
-
-      String[] oldMethods = (String[]) methodsField.get(null);
-      Set<String> methodsSet = new LinkedHashSet<>(Arrays.asList(oldMethods));
-      methodsSet.addAll(Arrays.asList(methods));
-      String[] newMethods = methodsSet.toArray(new String[0]);
-
-      methodsField.set(null /*static field*/, newMethods);
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      throw new IllegalStateException(e);
     }
   }
 
