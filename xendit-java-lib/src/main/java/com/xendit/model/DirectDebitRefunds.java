7abc6aeb5ec70fd3d83403962656549f7164120d
Glenda Emanuella Sutanto <glendaes0103@gmail.com>
2021-07-16 15:06:26 +0700

    feat(direct-debit): create models for direct debit payment

---
+++ xendit-java-lib/src/main/java/com/xendit/model/DirectDebitRefunds.java
@@ -0,0 +1,20 @@
+package com.xendit.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Setter
+@Getter
+public class DirectDebitRefunds {
+  @SerializedName("data")
+  private String[] data;
+
+  @SerializedName("has_more")
+  private Boolean hasMore;
+
+  @SerializedName("url")
+  private String url;
+}
